5abfc93e88766f6811cf955c3346e3a8c8e8066d
Riccardo Agnoletto <agnolettoriccardo@gmail.com>
2016-02-17 11:19:34 +0100

    Fix: fallback to example config if none found

--- index.js
+++ index.js
@@ -8,20 +8,24 @@ var mongoose = require('mongoose');
 var remove = require('./api/delete');
 var findConfig = require('find-config');
 
-var configFile = findConfig('simple-grid-config.js', { dir: 'config' });
+var configFile = findConfig('simple-grid-config.js', {
+  dir: 'config'
+}) || findConfig('simple-grid-config.example.js', {
+  dir: 'config'
+});
 var config = require(configFile);
 
 mongoose.connect(config.MONGODB_URL);
 
 module.exports = {
-	createNewGrid: create.createNewGrid,
-	createShape: create.createShape,
-	removeGrid: remove.removeGrid,
-	removeShape: remove.removeShape,
-	putObj: put.putObj,
-	getVector: get.getVector,
-	moveObj: update.moveObj,
-	findAndPopulate: get.findAndPopulate,
-	findDocById: get.findDocById,
-	searchDoc: get.searchDoc
+  createNewGrid: create.createNewGrid,
+  createShape: create.createShape,
+  removeGrid: remove.removeGrid,
+  removeShape: remove.removeShape,
+  putObj: put.putObj,
+  getVector: get.getVector,
+  moveObj: update.moveObj,
+  findAndPopulate: get.findAndPopulate,
+  findDocById: get.findDocById,
+  searchDoc: get.searchDoc
 };
